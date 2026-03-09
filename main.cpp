79f675593d59cfd671aaf3b7011f0e05c254928d
AdrianMTZVazquez <a.martin.quez@gmail.com>
2026-03-09 12:06:28 -0600

    Test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <string>
 
 int main(int, char**){
     std::cout << "Hello, from GitTest!\n";
+
+    std::string name;
+    std::cout << "Please enter your name: ";
+    std::getline(std::cin, name);
+    std::cout << "Hello, " << name << "!\n";
 }
